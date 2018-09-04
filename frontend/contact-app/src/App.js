99ea2229dad706eab46d83b41abcd244d1b781e7
Justin Powell <justinmpowell@knights.ucf.edu>
2018-09-04 02:16:13 -0400

    ui, some functionality, routes

--- frontend/contact-app/src/App.js
+++ frontend/contact-app/src/App.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './Components/Login';
+import Home from './Components/Home';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const App = () => {
+  return(
+    <BrowserRouter>
+      <Switch>
+        <Route exact path = "/" component={Login}/>
+        <Route path = "/login" component={Login}/>
+        <Route path = "/home" component={Home}/>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
